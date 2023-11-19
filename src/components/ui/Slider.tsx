70c330231cdaa6fb18bbc03d9ac0baf52f1ccb8e
MThorel <mthorel@corp.free.fr>
2023-11-19 12:40:48 +0100

    add icon for slider + slider > ok. Next step > handle 0 index

--- src/components/ui/Slider.tsx
+++ src/components/ui/Slider.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import HomeBanner from '../../assets/home-banner.webp'
+import Previous from '../../assets/previous-arrow.svg'
+import Next from '../../assets/newt-arrow.svg'
 
 type SliderProps = {
   images: string[]
@@ -10,28 +11,34 @@ const Slider = ({ images }: SliderProps) => {
 
   const goToPrevious = () => {
     const isFirstSlide = currentIndex === 0
-    const newIndex = isFirstSlide ? slides.length - 1 : currentIndex - 1
+    const newIndex = isFirstSlide ? images.length - 1 : currentIndex - 1
     setCurrentIndex(newIndex)
   }
 
   const goToNext = () => {
-    const isLastSlide = currentIndex === slides.length - 1
+    const isLastSlide = currentIndex === images.length - 1
     const newIndex = isLastSlide ? 0 : currentIndex + 1
     setCurrentIndex(newIndex)
   }
 
   return (
     <div className="slider">
-      <div className="arrow-group">
-        <div onClick={goToPrevious} className="left-arrow">
-          ❰
-        </div>
-        <div onClick={goToNext} className="right-arrow">
-          ❱
-        </div>
-      </div>
       <div className="inner-slider">
-        <img src={HomeBanner} alt="Slider" />
+        <div className="arrow-group">
+          <div onClick={goToPrevious}>
+            <img src={Previous} alt="go to previous image" className="left-arrow" />
+          </div>
+          <div onClick={goToNext}>
+            <img src={Next} alt="got to next image" className="right-arrow" />
+          </div>
+        </div>
+
+        <div>
+          <img src={`${images[currentIndex]}`} alt="location details" className="slider-img" />
+          <div className="pagination">
+            {currentIndex}/{images.length}
+          </div>
+        </div>
       </div>
     </div>
   )
