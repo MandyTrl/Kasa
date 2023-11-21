0325cd487e5b819eb7a893b9a74e286afc05cbb9
MThorel <mthorel@corp.free.fr>
2023-11-21 13:57:06 +0100

    install clsx & try animation for collapse

--- src/components/ui/Collapse.tsx
+++ src/components/ui/Collapse.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-import Previous from '../../assets/previous-arrow.svg'
-import Next from '../../assets/next-arrow.svg'
+import Down from '../../assets/previous-arrow.svg'
+import Up from '../../assets/next-arrow.svg'
+import clsx from 'clsx'
 
 type CollapseProps = {
   sectionName: string
@@ -10,18 +11,21 @@ type CollapseProps = {
 const Collapse = ({ section, sectionName }: CollapseProps) => {
   const handleDescription = typeof section === 'string'
   const [isOpen, setIsOpen] = useState<boolean>(false)
+  // tester la hauteur ET la hauteur max (pas d'impact sur le text) avec le translate de 0 à ?
 
   return (
     <div className="collapse">
       <div className="collapse-name-container">
         <p className="name-collapse">{sectionName}</p>
-        <img src={Next} alt="click to scroll down" onClick={() => setIsOpen(!isOpen)} />
+        <img src={isOpen ? Down : Up} alt="click to scroll down" onClick={() => setIsOpen(!isOpen)} />
       </div>
 
       {handleDescription ? (
-        <div className="details-collapse">{section}</div>
+        <div className={clsx(isOpen ? 'details-collapse-open' : 'details-collapse-closed', 'details-collapse')}>
+          {section}
+        </div>
       ) : (
-        <div className="details-collapse">
+        <div className={clsx(isOpen ? 'details-collapse-open' : 'details-collapse-closed', 'details-collapse')}>
           <ul>
             {section.map((el) => {
               return <li key={el}>{el}</li>
