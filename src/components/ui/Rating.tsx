ddf71ac92cb4cb25e99fb7acb4b04534b329fc98
MThorel <mthorel@corp.free.fr>
2023-11-23 16:04:46 +0100

    add uuid package for generate uniq key

--- src/components/ui/Rating.tsx
+++ src/components/ui/Rating.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { v4 as uuidv4 } from 'uuid'
 import ActiveStar from '../../assets/star-active.svg'
 import InactiveStar from '../../assets/star-inactive.svg'
 
@@ -9,12 +10,12 @@ type RatingProps = {
 const Rating = ({ rating }: RatingProps) => {
   const numberOfInactiveStars = 5 - rating
 
-  const activeStars = Array.from({ length: rating }, (index: number) => (
-    <img src={ActiveStar} alt="" aria-hidden="true" key={index} />
+  const activeStars = Array.from({ length: rating }, () => (
+    <img src={ActiveStar} alt="" aria-hidden="true" key={uuidv4()} />
   ))
 
-  const inactiveStars = Array.from({ length: numberOfInactiveStars }, (index: number) => (
-    <img src={InactiveStar} alt="" aria-hidden="true" key={index} />
+  const inactiveStars = Array.from({ length: numberOfInactiveStars }, () => (
+    <img src={InactiveStar} alt="" aria-hidden="true" key={uuidv4()} />
   ))
 
   return (
