e264f1fd19d5ba79485da21497a1ee9dd3e24d33
MThorel <mthorel@corp.free.fr>
2023-11-30 13:08:51 +0100

    responsive mobile : ok > tablet ?

--- src/components/ui/Collapse.tsx
+++ src/components/ui/Collapse.tsx
@@ -65,9 +65,7 @@ const Collapse = ({ sectionName, sectionContent, typeOfSection }: CollapseProps)
   return (
     <div className="collapse">
       <div className="collapse__name__container">
-        <p className={clsx(typeOfSection === CollapseTypeEnum.ABOUT && 'collapse__name__about', 'collapse__name')}>
-          {sectionName}
-        </p>
+        <p className="collapse__name">{sectionName}</p>
         <img src={isOpen ? Down : Up} alt="click to scroll down" onClick={() => setIsOpen(!isOpen)} />
       </div>
 
